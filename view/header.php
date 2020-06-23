fe9c4a73d59ee04433ed6ab55e1a4416a69ef721
keane-kane <abdouazizk50@gmail.com>
2020-06-23 19:48:11 +0200

    modification

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<div class="container">
+ <div class="header h-25 w-100">
+     <p class="text-center">SA GESTION</p>
+ </div>
+</div>
+
+<div class="container-fluid">
+    <div class="nav navbar">
+        <div class="narbar-link">
+            <ul>
+                <li><img src="" alt="" srcset=""><a href="#">Enregistrer Etudiant</a></li>
+                <li><img src="" alt=""><a href="#">Liste Etudiant</a> </li>
+                <li><img src="" alt=""><a href="#">Liste Chambre</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
